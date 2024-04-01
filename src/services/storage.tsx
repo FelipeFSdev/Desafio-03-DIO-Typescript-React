c7fb8609b3b4657887dd5e519b0c2b98d2a78639
Felipe Araújo <141762588+FelipeFSdev@users.noreply.github.com>
2024-04-01 16:40:14 -0300

    Create an user local storage

--- src/services/storage.tsx
+++ src/services/storage.tsx
@@ -1,12 +1,20 @@
 interface IDIoBank {
     login: boolean;
 }
+interface IUserData {
+    email: string,
+    password: string,
+    name: string,
+    balance: number,
+    id: number
+}
 
 const dioBank = {
     login: false
 }
+const userData = {}
 
-export const getAllLocalStorage = (): string | null  => {
+export const getAllLocalStorage = (): string | null => {
     return localStorage.getItem('diobank')
 }
 
@@ -16,4 +24,14 @@ export const createLocalStorage = (): void => {
 
 export const changeLocalStorage = (dioBank: IDIoBank): void => {
     localStorage.setItem('diobank', JSON.stringify(dioBank))
+}
+
+export const getUserData = (): string | null => {
+    return localStorage.getItem("userData")
+}
+export const createUserStorage = (): void => {
+    localStorage.setItem("userData", JSON.stringify(userData))
+}
+export const changeUserStorage = (userData: IUserData): void => {
+    return localStorage.setItem("userData", JSON.stringify(userData))
 }
